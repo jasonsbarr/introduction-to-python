aff8bd96dcfceca1f3b8e31c1204b0f35d73d651
Jason Barr <me@jasonbarr.dev>
2020-04-21 12:53:35 -0500

    Add finger exercises from EdX version of course

---
+++ problems/6.0001/finger-exercises/MITx/01-loops.py
@@ -0,0 +1,87 @@
+# Exercise: while exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some while loops.
+
+# 1. Convert the following into code that uses a while loop.
+
+# print 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints Goodbye!
+
+# Exercise: while exercise 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 2. Convert the following into code that uses a while loop.
+
+# prints Hello!
+# prints 10
+# prints 8
+# prints 6
+# prints 4
+# prints 2
+
+# Exercise: while exercise 3
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 3. Write a while loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we
+# define end to be 6, your code should print out the result:
+
+# 21
+# which is 1 + 2 + 3 + 4 + 5 + 6.
+
+# For problems such as these, do not include input statements or define variables we will provide for you. Our automating testing will
+# provide values so write your code in the following box assuming these variables are already defined.
+
+# Exercise: for exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some for loops.
+
+# 1. Convert the following code into code that uses a for loop.
+
+# prints 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints "Goodbye!"
+
+  
+# Exercise: for exercise 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 2. Convert the following code into code that uses a for loop.
+
+# prints "Hello!"
+# prints 10
+# prints 8
+# prints 6
+# prints 4
+# prints 2
+
+# Exercise: for exercise 3
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# 3. Write a for loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we
+# define end to be 6, your code should print out the result:
+# 21
+# which is 1 + 2 + 3 + 4 + 5 + 6.
+
+# For problems such as these, do not include input statements or define variables we will provide for you. Our automating testing will
+# provide values so write your code in the following box assuming these variables are already defined.
